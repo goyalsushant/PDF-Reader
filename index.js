778541a62c33a20858aae9604928a907b723dbb7
goyalsushant <goyalsushant2@gmail.com>
2019-09-29 13:49:05 +0530

    code updated

--- index.js
+++ index.js
@@ -5,12 +5,25 @@ var pdfReaderObj = new pdfReader.PdfReader
 const app = express()
 const port = 3000
 
-app.get('/',(req,res) => res.send('Hello World'))
+var rows = {}; // indexed by y-position
+ 
+function printRows() {
+  Object.keys(rows) // => array of y-positions (type: float)
+    .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
+    .forEach(y => console.log((rows[y] || []).join("")));
+}
+
+app.get('/', (req,res)=>{
+    pdfReaderObj.parseFileItems("sample_page_1.pdf", function(err,item) {
+        if (!item || item.page) {
+          // end of file, or page
+          printRows();
+          rows = {}; // clear rows for next page
+        } else if (item.text) {
+          // accumulate text items into rows object, per line
+          (rows[item.y] = rows[item.y] || []).push(item.text);
+        }
+      });
+})
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}`))
-
-pdfReaderObj.parseFileItems("sample.pdf", function(err, item) {
-    if (err) callback(err);
-    else if (!item) callback();
-    else if (item.text) console.log(item.text);
-  });
