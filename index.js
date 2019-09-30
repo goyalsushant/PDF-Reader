02bd8bc5da833375a0b0cc2c83e65970c1608813
goyalsushant <goyalsushant2@gmail.com>
2019-10-01 03:39:39 +0530

    array to string conversion

--- index.js
+++ index.js
@@ -8,22 +8,28 @@ const port = 3000
 var rows = {}; // indexed by y-position
  
 function printRows() {
+  var pageString = '';
   Object.keys(rows) // => array of y-positions (type: float)
     .sort((y1, y2) => parseFloat(y1) - parseFloat(y2)) // sort float positions
-    .forEach(y => console.log((rows[y] || []).join("")));
+    .forEach(y => {
+      pageString = pageString + (rows[y] ? rows[y] : []) + ' '
+    });
+    console.log(pageString.slice(pageString.search('Institution:')+ 'Institution:'.length, pageString.search('S.No.')))
 }
 
 app.get('/', (req,res)=>{
-    pdfReaderObj.parseFileItems("sample_page_1.pdf", function(err,item) {
-        if (!item || item.page) {
-          // end of file, or page
-          printRows();
-          rows = {}; // clear rows for next page
-        } else if (item.text) {
-          // accumulate text items into rows object, per line
-          (rows[item.y] = rows[item.y] || []).push(item.text);
-        }
-      });
+    res.send('Hello World !!')
 })
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}`))
+
+pdfReaderObj.parseFileItems("sample_page_1.pdf", function(err,item) {
+    if (!item || item.page) {
+      // end of file, or page
+      printRows();
+      rows = {}; // clear rows for next page
+    } else if (item.text) {
+      // accumulate text items into rows object, per line
+      (rows[item.y] = rows[item.y] || []).push(item.text);
+    }
+  });
